6dc322f8fcd40e8dcc0c7a8739e6d8fc541128be
Sam4uk <sto.ukr.net@gmail.com>
2023-07-10 22:37:12 +0300

    понеділок, 10 липня 2023 22:37:12 +0300

--- src/Approof/Approof.cpp
+++ src/Approof/Approof.cpp
@@ -11,18 +11,20 @@
 #include "Approof.hpp"
 #include "../NanitLib.h"
 
-const uint8_t k_blink_delay{80}; ///< Затримка між перемиканнями
-const uint8_t k_port_count{12}; ///< Кількість зовнішніх портів
-const uint8_t k_pin_count{4}; ///< кількість пінів в роз'ємах як ввикористовуються
+const uint8_t k_blink_delay = 80; ///< Затримка між перемиканнями
+const uint8_t k_port_count = 12; ///< Кількість зовнішніх портів
+const uint8_t k_pin_count = 4; ///< кількість пінів в роз'ємах як ввикористовуються
                               /// для обміну даними з перефірійними пристроями
 
 // #define APROOF_MODE                                                            \
 
 
 #define TOGGLE HIGH
-#define IGNORE_PIN                                                             \
-  (0xFF) //< Макровизначення мітка ігнорування піна. Перевірка роботи піна
-         //визначено у інший спосіб
+/**
+ * @brief Макровизначення мітка ігнорування піна. Перевірка роботи піна
+ * визначено у інший спосіб
+ */
+#define IGNORE_PIN (0xFF)
 #define RESET_PIN (IGNORE_PIN)
 
 void check_1(bool);
@@ -33,7 +35,7 @@ bool drive = 0;
 void PinTestSetup() {
   Serial.end();
 
-  for (uint8_t pin_to_set_mode{}; pin_to_set_mode < NUM_DIGITAL_PINS;
+  for (uint8_t pin_to_set_mode = 0; pin_to_set_mode < NUM_DIGITAL_PINS;
        pin_to_set_mode++) // Всі піни
   {
     if (pin_to_set_mode == TFT_BL // окрім підсітки дисплею
@@ -55,6 +57,7 @@ void PinTestSetup() {
 }
 void PinTestLoop() {
   // Карта пінів
+  // clang-format off
   const uint8_t PinMap[k_port_count][k_pin_count]{
       {IGNORE_PIN, IGNORE_PIN, IGNORE_PIN, IGNORE_PIN}, // port 1
       {P2_1, P2_2, P2_3, P2_4},                         // port 2
@@ -67,16 +70,16 @@ void PinTestLoop() {
       {P5_1, P5_2, P5_3, P5_4},                         // port 5
       {P6_1, P6_2, P6_3, P6_4},                         // port 6
       {P7_1, P7_2, P7_3, P7_4},                         // port 7
-      {1, 0, P8_3, RESET_PIN},                          // port 8
+      {P8_1, P8_2, P8_3, RESET_PIN},                    // port 8
       {P9_1, P9_2, P9_3, P9_4},                         // port 9
       {P10_1, P10_2, P10_3, P10_4},                     // port 10
-      {14, 15, P11_3, P11_4},                           // port 11
+      {P11_1, P11_2, P11_3, P11_4},                     // port 11
       {IGNORE_PIN, IGNORE_PIN, IGNORE_PIN, IGNORE_PIN}  // port 12
   };
-
+  // clang-format on
   check_1(drive);
-  for (uint8_t port{}; port < k_port_count; port++) // Перевіряємо кожен порт
-    for (uint8_t pin{}; pin < k_pin_count; pin++) { // Перевіряємо кожен пін
+  for (uint8_t port = 0; port < k_port_count; port++) // Перевіряємо кожен порт
+    for (uint8_t pin = 0; pin < k_pin_count; pin++) { // Перевіряємо кожен пін
       switch (PinMap[port][pin]) {
 #ifndef MEGACORE
       case 71: // Перевірка піна без MegaGore
@@ -103,6 +106,8 @@ void PinTestLoop() {
   drive = !drive;
 }
 
+/** @todo Переписати функції check_1 та check_2 таким чином щоб максимально
+ * перемістити перевірку пінів у `PinTestLoop`*/
 void check_1(bool what) {
   if (what) {
     digitalWrite(MOTOR1_A, !TOGGLE);
