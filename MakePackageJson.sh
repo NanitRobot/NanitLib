0607af31d2e92fd52f5c473208d48f5373b04267
Sam4uk <sto.ukr.net@gmail.com>
2023-11-01 11:09:05 +0200

    Оновлення плат

--- MakePackageJson.sh
+++ MakePackageJson.sh
@@ -43,6 +43,39 @@ cat <<EOF > "${OUT_FILE}"
         {
           "name": "Nanit",
           "architecture": "avr",
+          "version": "0.9",
+          "category": "Contributed",
+          "help": {
+            "online": "${HELP_ONLINE}"
+          },
+          "url": "${HOST}/${FILE_PATH}${FILE_NAME}",
+          "archiveFileName": "${FILE_NAME}",
+          "checksum" : "SHA-256:${SHA256}",
+          "size" : "${FILE_SIZE}",
+          "boards": [
+            {
+              "name": "Nanit Discovery 2.0"
+            },
+            {
+              "name": "Nanit Discovery 3"
+            },
+            {
+              "name": "Nanit Discovery 3.1"
+            },
+            {
+              "name": "Nanit Smart Home (Nanit 3.1 based)"
+            },
+            {
+              "name": "Nanit Smart Home (Nanit 3.3 based)"
+            },
+            {
+              "name": "Nanit Advanced"
+            } 
+          ]
+        },
+        {
+          "name": "Nanit",
+          "architecture": "avr",
           "version": "0.1.2",
           "category": "Contributed",
           "help": {
