11979ea7e12f537a90dab67ac47b9088c1ae93c0
Sam4uk <sto.ukr.net@gmail.com>
2023-11-07 10:14:00 +0200

    NanitCore update

--- PrepareFiles.sh
+++ PrepareFiles.sh
@@ -19,20 +19,24 @@ PINS_PATH="./Nanit/variants/Nanit"
 PINS_ARDUINO="pins_arduino.h"
 # Створюємо шлях для заголовчного файлц визначення пінів
 mkdir -p ${PINS_PATH}
-git submodule add https://github.com/NanitRobot/hex "${NANIT_PATH}/bootloaders"
+#git submodule add https://github.com/NanitRobot/hex "${NANIT_PATH}/bootloaders"
+cp cp binary/*.hex "${NANIT_PATH}/bootloaders/"
 # Копіюємо файл
 cp -ruf ./MegaCore/avr/variants/100-pin-arduino-mega/pins_arduino.h ${PINS_PATH}/${PINS_ARDUINO}
 # Бібліотеки 
 # TODO зробити красиво в циклі
 mkdir -p ${NANIT_LIBRARY_PATH}
-# HID
-# SPI
-# SoftwareSerial
-# Wire
+
+touch "${NANIT_LIBRARY_PATH}/touch"
+
 cp -ruf ${MEGACORE_LIBRARY_PATH}/EEPROM/ ${NANIT_LIBRARY_PATH}/EEPROM/
+# HID
 cp -ruf ${MEGACORE_LIBRARY_PATH}/HID/ ${NANIT_LIBRARY_PATH}/HID/
+# SPI
 cp -ruf ${MEGACORE_LIBRARY_PATH}/SPI/ ${NANIT_LIBRARY_PATH}/SPI/
+# SoftwareSerial
 cp -ruf ${MEGACORE_LIBRARY_PATH}/SoftwareSerial/ ${NANIT_LIBRARY_PATH}/SoftwareSerial/
+# Wire
 cp -ruf ${MEGACORE_LIBRARY_PATH}/Wire/ ${NANIT_LIBRARY_PATH}/Wire/
 # Шрифти
 cp -ruf ./Adafruit-GFX-Library/Fonts/ ${NANIT_LIBRARY_PATH}/Fonts/
@@ -43,7 +47,7 @@ cp -ruf ${MEGACORE_CORE_PATH}/ ${NANIT_CORE_PATH}/
 # КОСТИЛЬ
 mv ${NANIT_CORE_PATH}/MCUdude_corefiles "${NANIT_CORE_PATH}/MegaCore"
 
-rm -fr "${NANIT_PATH}/bootloaders/.git*"
+#rm -fr "${NANIT_PATH}/bootloaders/.git*"
 # Розпаковуємо підготовлені файли
 unzip -o NanitIDE.zip -d ${NANIT_PATH}
 # Пакуємо файл 
@@ -52,6 +56,6 @@ zip -qq -r NanitCore.zip ${NANIT_PATH}
 # Прибираємо за собою
 rm -fr ${NANIT_PATH}
 
-git submodule deinit -f MegaCore Adafruit-GFX-Library Nanit/bootloaders
+git submodule deinit -f MegaCore Adafruit-GFX-Library # Nanit/bootloaders
 git rm -fr ${MEGACORE_PATH}
 git rm -fr ./Adafruit-GFX-Library
