c9cd9454f80ebfa11438b5cdfc176cf23a9dc67e
Sam4uk <sto.ukr.net@gmail.com>
2024-03-25 14:41:51 +0200

    Автоматична нумерація версії

--- MakePackageJson.sh
+++ MakePackageJson.sh
@@ -46,7 +46,7 @@ cat <<EOF > "${OUT_FILE}"
         {
           "name": "Nanit",
           "architecture": "avr",
-          "version": "1.0.0",
+          "version": "1.0.$(date +%y%m%d)",
           "category": "Contributed",
           "help": {
             "online": "${HELP_ONLINE}"
