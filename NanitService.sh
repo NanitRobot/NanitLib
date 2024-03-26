76aa395dbaf98bf00b9d53d0ff54e510284d6eb3
Sam4uk <sto.ukr.net@gmail.com>
2024-03-26 16:01:34 +0200

    Add files via upload

--- NanitService.sh
+++ NanitService.sh
@@ -42,7 +42,7 @@ cat <<EOF > "${OUT_FILE}"
         "online": "${HELP_ONLINE}"
       },
       "platforms": [
-        {
+                {
           "name": "NanitService(NanitService)",
           "architecture": "avr",
           "version": "$(date +%-y.%-m.%-d)",
@@ -58,125 +58,72 @@ cat <<EOF > "${OUT_FILE}"
             {
               "name": "SERVICE NANIT BARDS"
             }
-          ],
-          "toolsDependencies": [
+          ]
+        }
+      ]
+    }
+  ],
+  "toolsDependencies": [
             {
               "packager": "arduino",
               "name": "avr-gcc",
               "version": "7.3.0-atmel3.6.1-arduino7"
             },
             {
-              "packager": "NanitService",
-              "name": "avrdude",
-              "version": "7.2-arduino.1"
-            },
-            {
               "packager": "arduino",
-              "name": "arduinoOTA",
-              "version": "1.3.0"
+              "name": "avrdude",
+              "version": "6.3.0-arduino17"
             }
-          ]
-        }
-      ],
-      "tools": [
+          ],
+          "tools": [
         {
-          "name": "avrdude",
-          "version": "7.1-arduino.1",
+          "name": "avr-gcc",
+          "version": "7.3.0-atmel3.6.1-arduino7",
           "systems": [
             {
-              "size": "1161418",
-              "checksum": "SHA-256:6c842593530e8ee463760fca6c6244eeff5718fac6ed5798386a2d9704735fb5",
+              "size": "34683056",
+              "checksum": "SHA-256:3903553d035da59e33cff9941b857c3cb379cb0638105dfdf69c97f0acc8e7b5",
               "host": "arm-linux-gnueabihf",
-              "archiveFileName": "avrdude_7.1-arduino.1_Linux_ARMv6.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.1-arduino.1_Linux_ARMv6.tar.gz"
+              "archiveFileName": "avr-gcc-7.3.0-atmel3.6.1-arduino7-arm-linux-gnueabihf.tar.bz2",
+              "url": "http://downloads.arduino.cc/tools/avr-gcc-7.3.0-atmel3.6.1-arduino7-arm-linux-gnueabihf.tar.bz2"
             },
             {
-              "size": "1261196",
-              "checksum": "SHA-256:37743def85b0fa70b566797d889e6d649f644ad7fa67a3e151b6e61aba9a0d0a",
+              "size": "38045723",
+              "checksum": "SHA-256:03d322b9df6da17289e9e7c6233c34a8535d9c645c19efc772ba19e56914f339",
               "host": "aarch64-linux-gnu",
-              "archiveFileName": "avrdude_7.1-arduino.1_Linux_ARM64.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.1-arduino.1_Linux_ARM64.tar.gz"
+              "archiveFileName": "avr-gcc-7.3.0-atmel3.6.1-arduino7-aarch64-pc-linux-gnu.tar.bz2",
+              "url": "http://downloads.arduino.cc/tools/avr-gcc-7.3.0-atmel3.6.1-arduino7-aarch64-pc-linux-gnu.tar.bz2"
             },
             {
-              "size": "834101",
-              "checksum": "SHA-256:436a7a5f467774cdd39683d2226314ba90485b0b08c629602004f4bbfdddf575",
-              "host": "x86_64-apple-darwin12",
-              "archiveFileName": "avrdude_7.1-arduino.1_macOS_64bit.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.1-arduino.1_macOS_64bit.tar.gz"
+              "size": "36684546",
+              "checksum": "SHA-256:f6ed2346953fcf88df223469088633eb86de997fa27ece117fd1ef170d69c1f8",
+              "host": "x86_64-apple-darwin14",
+              "archiveFileName": "avr-gcc-7.3.0-atmel3.6.1-arduino7-x86_64-apple-darwin14.tar.bz2",
+              "url": "http://downloads.arduino.cc/tools/avr-gcc-7.3.0-atmel3.6.1-arduino7-x86_64-apple-darwin14.tar.bz2"
             },
             {
-              "size": "1169286",
-              "checksum": "SHA-256:11f6c709d9f950e35993ca27ed870a86711d7093255a9e4b4463160f1778fd19",
-              "host": "x86_64-linux-gnu",
-              "archiveFileName": "avrdude_7.1-arduino.1_Linux_64bit.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.1-arduino.1_Linux_64bit.tar.gz"
-            },
-            {
-              "size": "1170712",
-              "checksum": "SHA-256:f5eff5141c9385b56ca3be780453149c912abf7b5bf87de8398f661a1369d305",
-              "host": "i686-linux-gnu",
-              "archiveFileName": "avrdude_7.1-arduino.1_Linux_32bit.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.1-arduino.1_Linux_32bit.tar.gz"
-            },
-            {
-              "size": "1591591",
-              "checksum": "SHA-256:35d3b917573a91b7a7e5805919a1fa3bc033541e2941b710bfc627071c738731",
+              "size": "52519412",
+              "checksum": "SHA-256:a54f64755fff4cb792a1495e5defdd789902a2a3503982e81b898299cf39800e",
               "host": "i686-mingw32",
-              "archiveFileName": "avrdude_7.1-arduino.1_Windows_32bit.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.1-arduino.1_Windows_32bit.tar.gz"
-            }
-          ]
-        },
-        {
-          "name": "avrdude",
-          "version": "7.2-arduino.1",
-          "systems": [
-            {
-              "size": "1243922",
-              "checksum": "SHA-256:b5b88e4f52c0edb861f5ae54acbd752f9ecd562a4db4d821ab8c276ee5696dab",
-              "host": "arm-linux-gnueabihf",
-              "archiveFileName": "avrdude_7.2-arduino.1_Linux_ARMv6.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.2-arduino.1_Linux_ARMv6.tar.gz"
+              "archiveFileName": "avr-gcc-7.3.0-atmel3.6.1-arduino7-i686-w64-mingw32.zip",
+              "url": "http://downloads.arduino.cc/tools/avr-gcc-7.3.0-atmel3.6.1-arduino7-i686-w64-mingw32.zip"
             },
             {
-              "size": "1348250",
-              "checksum": "SHA-256:a3862e6a38668c2688dc0822b16c3f824612cc606259b6813bc5778d36c92ba9",
-              "host": "aarch64-linux-gnu",
-              "archiveFileName": "avrdude_7.2-arduino.1_Linux_ARM64.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.2-arduino.1_Linux_ARM64.tar.gz"
-            },
-            {
-              "size": "928946",
-              "checksum": "SHA-256:256cbde856714a18c11b4c99f6b00eed65e4208c92425fa9cfd75a5bdfb6ab09",
-              "host": "x86_64-apple-darwin12",
-              "archiveFileName": "avrdude_7.2-arduino.1_macOS_64bit.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.2-arduino.1_macOS_64bit.tar.gz"
-            },
-            {
-              "size": "1256881",
-              "checksum": "SHA-256:25e1b568757d9a58b9663e4493ffc04b9e6d690535c9c1e6c1db7d1ecffb5eff",
-              "host": "x86_64-linux-gnu",
-              "archiveFileName": "avrdude_7.2-arduino.1_Linux_64bit.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.2-arduino.1_Linux_64bit.tar.gz"
-            },
-            {
-              "size": "1252284",
-              "checksum": "SHA-256:86a811f6ba2bebbb717a524fcff495f0ebb146abcd15d341a10d867b8e8c83fd",
+              "size": "37176991",
+              "checksum": "SHA-256:954bbffb33545bcdcd473af993da2980bf32e8461ff55a18e0eebc7b2ef69a4c",
               "host": "i686-linux-gnu",
-              "archiveFileName": "avrdude_7.2-arduino.1_Linux_32bit.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.2-arduino.1_Linux_32bit.tar.gz"
+              "archiveFileName": "avr-gcc-7.3.0-atmel3.6.1-arduino7-i686-pc-linux-gnu.tar.bz2",
+              "url": "http://downloads.arduino.cc/tools/avr-gcc-7.3.0-atmel3.6.1-arduino7-i686-pc-linux-gnu.tar.bz2"
             },
             {
-              "size": "1682746",
-              "checksum": "SHA-256:5536c9fcb41f4a36aa55b3711a0d74943a401261b15cc8aae2473c30ff292021",
-              "host": "i686-mingw32",
-              "archiveFileName": "avrdude_7.2-arduino.1_Windows_32bit.tar.gz",
-              "url": "http://downloads.arduino.cc/tools/avrdude_7.2-arduino.1_Windows_32bit.tar.gz"
+              "size": "37630618",
+              "checksum": "SHA-256:bd8c37f6952a2130ac9ee32c53f6a660feb79bee8353c8e289eb60fdcefed91e",
+              "host": "x86_64-linux-gnu",
+              "archiveFileName": "avr-gcc-7.3.0-atmel3.6.1-arduino7-x86_64-pc-linux-gnu.tar.bz2",
+              "url": "http://downloads.arduino.cc/tools/avr-gcc-7.3.0-atmel3.6.1-arduino7-x86_64-pc-linux-gnu.tar.bz2"
             }
           ]
         }
       ]
-    }
-  ]
 }
 EOF
