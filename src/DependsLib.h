73aea0dd53b6f475eddb1b381a083566ee9ce8aa
SergiyK <kireevsergiy7@gmail.com>
2026-01-21 11:42:14 +0200

    Comment FastLed_NeoPixel

--- src/DependsLib.h
+++ src/DependsLib.h
@@ -19,7 +19,7 @@
 #  error "Include <NanitLib.h> instead of this file."
 #endif
 
-#include <FastLED_NeoPixel.h> 
+//#include <FastLED_NeoPixel.h> 
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 #include <EEPROM.h>
